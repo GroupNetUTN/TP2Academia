46dba56c2b4d7aa15e5ac18d8261a0f411a7d8a5
matiasfic <matiasfic@hotmail.com>
2014-09-07 12:46:11 -0300

    CursoLogic.cs LISTO

---
+++ Business.Logic/CursoLogic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Business.Entities;
+using Data.Database;
+
+namespace Business.Logic
+{
+    class CursoLogic : BusinessLogic
+    {
+        private CursoAdapter _CursoData;
+
+        public CursoLogic()
+        {
+            _CursoData = new CursoAdapter();
+        }
+
+        public CursoAdapter CursoData
+        {
+            get { return _CursoData; }
+        }
+
+        public List<Curso> GetAll()
+        {
+            return CursoData.GetAll();
+        }
+
+        public Curso GetOne(int ID)
+        {
+            return CursoData.GetOne(ID);
+        }
+
+        public void Delete(int ID)
+        {
+            CursoData.Delete(ID);
+        }
+
+        public void Save(Curso cur)
+        {
+            CursoData.Save(cur);
+        }
+    }
+}
